aaff92d36ed905ee324f07acab1939d0c2adb1af
HardDie <oleg1995sysoev@yandex.ru>
2024-07-02 12:24:22 +0400

    Added version

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -10,7 +9,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
+var Version string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -29,7 +28,8 @@ to quickly create a Cobra application.`,
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
+func Execute(v string) {
+	Version = v
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
@@ -47,5 +47,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "github.com/HardDie/LibraryHashCheck/cmd"
+import (
+	"runtime/debug"
+
+	"github.com/HardDie/LibraryHashCheck/cmd"
+)
+
+var Version = "dev"
 
 func main() {
-	cmd.Execute()
+	if info, available := debug.ReadBuildInfo(); available {
+		switch info.Main.Version {
+		case "", "(devel)":
+			// skip
+		default:
+			Version = info.Main.Version
+		}
+	}
+	cmd.Execute(Version)
 }
